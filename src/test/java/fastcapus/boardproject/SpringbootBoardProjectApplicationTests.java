3c768ef5b24fa055e4baa008c904c505d202cad4
o-rchid <ozzve35@gmail.com>
2024-05-12 23:35:06 +0900

    #8 - 스프링 부트 프로젝트 시작 스프링 부트 프로젝트 초기화 들어간 초기화 기술: * 스프링 웹 * 액츄에이터 * 데브툴즈 * 롬복

---
+++ src/test/java/fastcapus/boardproject/SpringbootBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcapus.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
