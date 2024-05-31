28bc3a3a1b59d489621c334e7c4c10fd4a366715
o-rchid <ozzve35@gmail.com>
2024-05-31 23:51:51 +0900

    #12 - Repository 테스트 코드 수정

--- src/test/java/fastcampus/boardproject/repository/JpaRepositoryTest.java
+++ src/test/java/fastcampus/boardproject/repository/JpaRepositoryTest.java
@@ -1,18 +1,22 @@
 package fastcampus.boardproject.repository;
 
+import fastcampus.boardproject.SpringbootBoardProjectApplication;
 import fastcampus.boardproject.config.JpaConfig;
+import fastcampus.boardproject.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJdbcTest
-
 class JpaRepositoryTest {
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
@@ -28,6 +32,14 @@ class JpaRepositoryTest {
     @DisplayName("select 테스트")
     @Test
     void givenTestData_whenSelecting_thenWorksFine(){
-         
+         // Given
+
+         // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(0);
     }
 }
